950d625fa9a84b69f0faa2473ac63bb9b81f22b7
AbdulRehman942004 <abdulrehmanrizwan942004@gmail.com>
2026-04-07 22:14:42 +0500

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG
+
+### v1.0.1 - 04/07/2026
+- Update `requirements.txt`
+
+### v1.0.0 - 04/07/2026
+- Codebase setup
+
--- server/database.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import create_engine
-
-DATABASE_URL = "sqlite:///trademate.db"
-
-engine = create_engine(DATABASE_URL, echo=True)
